3901b145f20622fce339dcf949f76c50550636c8
ReinaIsabelNavarreteOlivares <20323802@clases.edu.sv>
2026-05-29 18:57:28 -0600

    Create condcionales.py

---
+++ practica2/practica3/practica4/practica5/practica6/practica7/practica8/condcionales.py
@@ -0,0 +1,16 @@
+print("Escribe tu nombre:")
+nombre  = input()
+#print("Escribe tu edad")
+#edad=int(input())
+
+#elif
+#operadores lógicos
+#and(y)/ or (o)
+#and:todas las expresiones sean true
+#or:con que una de las expresiones sea true
+#> <     >:   <=
+
+if nombre == "Rodrigo" or nombre =="santiago": 
+    print("Me gusta tu nombre")
+else:
+    print("que nombre tan extraño")
