be785b34400c56220d7a9e2f4059404dc1454de7
ReinaIsabelNavarreteOlivares <20323802@clases.edu.sv>
2026-05-29 18:58:22 -0600

    Create condicionales.py

---
+++ practica2/practica3/practica4/practica5/practica6/practica7/practica8/practica9/condicionales.py
@@ -0,0 +1,11 @@
+#Lógico Boolean Booleano 
+#Verdedaro /Falso:true/false
+#dato = 5 == 5
+#> < ==
+print("ingresa tu nombre")
+nombre = input()
+
+if nombre == "Isabel":
+    print("Bienvenido Isabel")
+else:
+    print("Que nombre tan extraño")
