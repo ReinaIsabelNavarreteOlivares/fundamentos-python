a2f82c6edb0251b2921fc852f1282c20133eaa78
ReinaIsabelNavarreteOlivares <20323802@clases.edu.sv>
2026-05-30 13:38:10 -0600

    Add dictionary example with key-value pairs

---
+++ practica13/diccionario.py
@@ -0,0 +1,12 @@
+#lista de elementos llave:valor
+#arreglos asociativos
+
+persona={"nombre":"Rodrigo",
+         "edad":800,
+         "apellido":"montemayor"}
+
+persona["apodo"]="Ringa Tech"
+
+#print(persona.items())
+
+print("diversión" in persona)
