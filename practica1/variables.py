923776e17b8fce38eddfc2205a62380ccacea158
ReinaIsabelNavarreteOlivares <20323802@clases.edu.sv>
2026-05-29 18:47:30 -0600

    Create variables.py

---
+++ practica1/variables.py
@@ -0,0 +1,14 @@
+#Soy un comentario
+#strings,cadenas,textos
+#numericos , int (integer, entero)float(flotante)
+nombre="Roberto"
+apellido="Giraldo"
+edad=35
+
+print(nombre,apellido,edad)
+
+#edad=edad/3
+
+print(nombre,apellido,edad)
+
+print(type(nombre))
